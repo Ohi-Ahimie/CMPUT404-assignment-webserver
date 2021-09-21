b46c02e473dcee3359442e71dbce705ba32dc0bf
Student <ohi-ii@att.net>
2021-09-21 17:55:10 -0400

    done except for mime stuff

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 #  coding: utf-8 
 import socketserver
+import re
+import urllib.parse
+from enum import Enum, auto
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,11 +32,104 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
+    requestLine = []
     
+    class ServerState(Enum):
+        READ_DATA = auto()
+        INVALID_REQUEST = auto()
+        READ_METHOD = auto()
+        INVALID_METHOD = auto()
+        CHECK_URI = auto()
+        FIX_URI = auto()
+        INVALID_URI = auto()
+        SERVE_DATA = auto()
+        TERMINATE = auto()
+
+    def checkData(self):
+        try:
+            print(self.data)
+            dataLines = re.split('\n|\r\n', self.data.decode('utf-8'))
+        except Exception as e:
+            print('An error occured: ' + e)
+            return self.ServerState.INVALID_REQUEST
+
+        requestLine = dataLines[0].split()
+        if len(requestLine) != 3:
+            requestLine = []
+            return self.ServerState.INVALID_REQUEST
+        else:
+            self.requestLine = requestLine
+            return self.ServerState.READ_METHOD
+
+    def checkMethod(self):
+        if self.requestLine[0] == 'GET':
+            return self.ServerState.CHECK_URI
+        elif self.requestLine[0] == 'OPTIONS' or self.requestLine[0] == 'HEAD' or self.requestLine[0] == 'POST' or self.requestLine[0] == 'PUT' or self.requestLine[0] == 'DELETE' or self.requestLine[0] == 'TRACE' or self.requestLine[0] == 'CONNECT':
+            return self.ServerState.INVALID_METHOD
+        else:
+            return self.ServerState.INVALID_REQUEST
+
+    def checkUri(self):
+        path = self.requestLine[1]
+        if '..' in path:
+            return self.ServerState.INVALID_URI
+        
+        pathLocated = os.path.isdir('./www' + path) 
+        if (pathLocated and path.endswith('/')) or (os.path.isfile('./www' + path)):
+            return self.ServerState.SERVE_DATA
+        elif pathLocated:
+            return self.ServerState.FIX_URI
+
+        return self.ServerState.INVALID_URI
+    
+    def handle301(self):
+        path = self.requestLine[1]
+        self.request.sendall(bytearray("HTTP/1.1 301 Moved Permanently\r\nLocation: http://127.0.0.1:8080" + path + '/\r\n' + "\r\n",'utf-8'))
+        return self.ServerState.TERMINATE
+
+    def handle400(self):
+        self.request.sendall(bytearray("HTTP/1.1 400 Bad Request\r\n\r\n",'utf-8'))
+        return self.ServerState.TERMINATE
+
+    def handle405(self):
+        self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\r\n\r\n",'utf-8'))
+        return self.ServerState.TERMINATE
+
+    def handle404(self):
+        self.request.sendall(bytearray("HTTP/1.1 404 Page Not Found\r\n\r\n",'utf-8'))
+        return self.ServerState.TERMINATE
+
+    def serve(self):
+        path = self.requestLine[1]
+        if os.path.isdir('./www' + path):
+            f = open('./www' + path + 'index.html')
+            self.request.sendall(bytearray("HTTP/1.1 200 OK\r\n\r\n" + f.read(),'utf-8'))
+            f.close()
+        else:
+            f = open('./www' + path)
+            self.request.sendall(bytearray("HTTP/1.1 200 OK\r\n\r\n" + f.read(),'utf-8'))
+            f.close()
+        return self.ServerState.TERMINATE
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        
+        switcher = {
+            self.ServerState.READ_DATA : self.checkData,
+            self.ServerState.INVALID_REQUEST : self.handle400,
+            self.ServerState.READ_METHOD : self.checkMethod,
+            self.ServerState.INVALID_METHOD : self.handle405,
+            self.ServerState.CHECK_URI : self.checkUri,
+            self.ServerState.FIX_URI : self.handle301,
+            self.ServerState.INVALID_URI : self.handle404,
+            self.ServerState.SERVE_DATA : self.serve           
+        }
+
+        state = self.ServerState.READ_DATA
+        while state is not self.ServerState.TERMINATE:
+            func = switcher.get(state)
+            state = func()
+            print(state)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
